b447a00e844731831a4ec36b448d65d948aaf451
i.kłusek <J6ZkqwVUn>
2024-06-26 22:25:02 +0200

    NS-14-Simple page for notifications

--- src/pages/NewFeature/NewFeature.tsx
+++ src/pages/NewFeature/NewFeature.tsx
@@ -1,8 +1,9 @@
 import { useParams } from 'react-router-dom';
 import './NewFeature.module.scss';
+import NotificationPage from '../../components/Notifications/NotificationPage/NotificationPage';
 
 export default function NewFeature() {
   const id = useParams().id as string;
 
-  return <p>{id}</p>;
+  return <NotificationPage id={id} />;
 }
