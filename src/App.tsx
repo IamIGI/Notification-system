aae878df53e605c9305c564f648f447bccdf38cf
i.kłusek <J6ZkqwVUn>
2024-06-26 16:48:26 +0200

    NS-4-Added default modal functionallity and modal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { useState } from 'react';
 import NavBar from './components/NavBar/NavBar';
+import NotificationModal from './components/Notifications/Modal/NotificationModal';
 
 export default function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function openNotificationModal() {
+    setIsOpen(true);
+  }
   return (
     <>
-      <NavBar />
+      <NavBar openNotification={openNotificationModal} />
+      <NotificationModal open={isOpen} onClose={() => setIsOpen(false)} />
     </>
   );
 }
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,19 +2,25 @@ import styles from './NavBar.module.scss';
 import companyLogo from '../../assets/images/mindpal.webp';
 import bellIcon from '../../assets/images/bell_icon.svg';
 
-export default function NavBar() {
-  return (
-    <nav className={styles.navbar}>
-      <img
-        src={companyLogo}
-        alt="Company Logo"
-        className={styles.company_logo}
-      />
+interface NavBarProps {
+  openNotification: () => void;
+}
 
-      <button className={styles.button}>
-        <img src={bellIcon} alt="Company Logo" className={styles.logo} />
-        <span className={styles.indicator}>1</span>
-      </button>
-    </nav>
+export default function NavBar({ openNotification }: NavBarProps) {
+  return (
+    <>
+      <nav className={styles.navbar}>
+        <img
+          src={companyLogo}
+          alt="Company Logo"
+          className={styles.company_logo}
+        />
+
+        <button className={styles.button} onClick={() => openNotification()}>
+          <img src={bellIcon} alt="Company Logo" className={styles.logo} />
+          <span className={styles.indicator}>1</span>
+        </button>
+      </nav>
+    </>
   );
 }
